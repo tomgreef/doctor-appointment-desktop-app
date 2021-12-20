2ca81fe573351582ec362a70251b1bdccbdbf4e6
Tom van Greevenbroek <tom.v.g@outlook.com>
2021-12-20 10:05:26 +0100

    Parsa no sabe

--- GESDAPPER/Revisiones.cs
+++ GESDAPPER/Revisiones.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace GESDAPPER
 {
-    public partial class Form1 : Form
+    public partial class Revisiones : Form
     {
-        public Form1()
+        public Revisiones()
         {
             InitializeComponent();
         }
