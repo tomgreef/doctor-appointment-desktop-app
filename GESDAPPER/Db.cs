ac6d9cb94dfba26cdce582d9f6370188c72a2aaa
Charly6596 <hello@carlosj.me>
2022-01-09 14:58:07 +0100

    Implement db operations

---
+++ GESDAPPER/Db.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using Dapper.Contrib.Extensions;
+using System.Data.SqlClient;
+
+
+namespace GESDAPPER
+{
+    public class Db
+    {
+        private static readonly string _connectionString = Properties.Settings.Default.ConnectionString;
+
+        public void Save<T>(T elem) where T : class
+        {
+            Query(c => c.Insert(elem));
+        }
+
+        public T GetById<T>(string id) where T : class
+        {
+            return Query(c => c.Get<T>(id));
+        }
+
+        public void Update<T>(T elem) where T: class
+        {
+            Query(c => c.Update(elem));
+        }
+
+        public void Delete<T>(T elem) where T : class
+        {
+            Query(c => c.Delete(elem));
+        }
+
+        public IEnumerable<T> GetAll<T>() where T : class
+        {
+           return Query(c => c.GetAll<T>());
+        }
+
+        private void Query(Action<SqlConnection> predicate)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                predicate(connection);
+            }
+        }
+
+        private IEnumerable<T> Query<T>(Func<SqlConnection, IEnumerable<T>> predicate)
+        {
+            IEnumerable<T> res;
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                res = predicate(connection);
+            }
+
+            return res;
+        }
+
+        private T Query<T>(Func<SqlConnection, T> predicate)
+        {
+            T res;
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                res = predicate(connection);
+            }
+
+            return res;
+        }
+    }
+}
---
+++ GESDAPPER/Eye.cs
@@ -0,0 +1,27 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GESDAPPER
+{
+    [Table("tEye")]
+    class Eye
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public Client Cliente { get; set; }
+        public string Consulta { get; set; }
+        public double OdEsfera { get; set; }
+        public double OdCilindro { get; set; }
+        public double OdAdicion { get; set; }
+        public double OdAgudeza { get; set; }
+        public double OiEsfera { get; set; }
+        public double OiCilindro { get; set; }
+        public double OiAdicion { get; set; }
+        public double OiAgudeza { get; set; }
+    }
+}
